5c8bf67d582e9fd08750b551ca38be7d06373b79
KimJRan <116188777+KimJRan@users.noreply.github.com>
2023-04-01 00:08:32 +0900

    3월 30일

---
+++ Chapter08/src/sec01/ex04/FirstServlet.java
@@ -0,0 +1,28 @@
+package sec01.ex04;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/first4")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		response.sendRedirect("second4?name=kim");
+	}
+
+}
